454cea82da3fd159b20f6a7cde6efea3424ffa94
John Cilfford R. Madriñan <johncliffordmadrinan838@gmail.com>
2022-01-20 16:40:49 +0700

    Create a new Tasks List Component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+function TasksList()
+{
+    return (
+        <ul>
+            <li className="tasks-item">Follow Edukasyon.ph on Facebook.</li>
+            <li className="tasks-item">Follow AWS Siklab Pilipinas on Facebook.</li>
+            <li className="tasks-item">Follow Zuitt Coding Bootcamp on Facebook.</li>
+        </ul>
+    );
+}
+
 function App()
 {
     const inlineStyle = {
@@ -14,11 +25,7 @@ function App()
                 <h1>React JS - Todo List App</h1>
                 <p style={ inlineStyle }>The most simple and amazing todo-list React app.</p>
 
-                <ul>
-                    <li className="tasks-item">Follow Edukasyon.ph on Facebook.</li>
-                    <li className="tasks-item">Follow AWS Siklab Pilipinas on Facebook.</li>
-                    <li className="tasks-item">Follow Zuitt Coding Bootcamp on Facebook.</li>
-                </ul>
+                <TasksList/>
             </header>
         </div>
     );
