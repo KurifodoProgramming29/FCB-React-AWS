9fc3cdb1b4ba042b6083cf70af2edb6145aaab33
John Cilfford R. Madriñan <johncliffordmadrinan838@gmail.com>
2022-01-20 16:27:02 +0700

    Add some CSS styles and implement inline style

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import "./index.css";
 
 function App()
 {
+    const inlineStyle = {
+        "marginBottom": "15px"
+    };
+
     return (
         <div className="app">
             <header className="app-header">
                 <h1>React JS - Todo List App</h1>
-                <p>The most simple and amazing todo-list React app.</p>
+                <p style={ inlineStyle }>The most simple and amazing todo-list React app.</p>
+
+                <ul>
+                    <li className="tasks-item">Follow Edukasyon.ph on Facebook.</li>
+                    <li className="tasks-item">Follow AWS Siklab Pilipinas on Facebook.</li>
+                    <li className="tasks-item">Follow Zuitt Coding Bootcamp on Facebook.</li>
+                </ul>
             </header>
         </div>
     );
